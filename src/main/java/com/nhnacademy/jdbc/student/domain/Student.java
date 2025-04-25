58e15302953d61c76950fde6184fdbcfe7437239
RiveroadKim <kanggil0825@gmail.com>
2025-04-25 09:45:57 +0900

    crud success

--- src/main/java/com/nhnacademy/jdbc/student/domain/Student.java
+++ src/main/java/com/nhnacademy/jdbc/student/domain/Student.java
@@ -1,5 +1,8 @@
 package com.nhnacademy.jdbc.student.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.time.LocalDateTime;
 import java.util.Objects;
 
@@ -8,6 +11,7 @@ public class Student {
     public enum GENDER{
         M,F
     }
+
     private final String id;
     private final String name;
     private final GENDER gender;
@@ -15,4 +19,35 @@ public class Student {
     private final LocalDateTime createdAt;
 
     //todo#0 필요한 method가 있다면 추가합니다.
+    public Student(String id, String name, GENDER gender, Integer age) {
+        this.id = id;
+        this.name = name;
+        this.gender = gender;
+        this.age = age;
+        createdAt = LocalDateTime.now();
+    }
+
+    public Student(String id, String name, GENDER gender, Integer age, LocalDateTime createdAt) {
+        this.id = id;
+        this.name = name;
+        this.gender = gender;
+        this.age = age;
+        this.createdAt = createdAt;
+    }
+
+    public String getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public GENDER getGender() {
+        return gender;
+    }
+    public Integer getAge() {
+        return age;
+    }
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
 }
--- src/main/java/com/nhnacademy/jdbc/student/repository/impl/StatementStudentRepository.java
+++ src/main/java/com/nhnacademy/jdbc/student/repository/impl/StatementStudentRepository.java
@@ -17,29 +17,83 @@ public class StatementStudentRepository implements StudentRepository {
     @Override
     public int save(Student student){
         //todo#1 insert student
+        String query = String.format("INSERT INTO jdbc_students(id,name,gender,age) values('%s', '%s', '%s', %d)",
+                student.getId(),
+                student.getName(),
+                student.getGender(),
+                student.getAge()
+        );
+        log.debug("Insert query: {}", query);
 
-        return 0;
+        try(Connection conn = DbUtils.getConnection();
+            Statement statement = conn.createStatement();) {
+            int result = statement.executeUpdate(query);
+            log.debug("Insert result: {}", result);
+            return result;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public Optional<Student> findById(String id){
         //todo#2 student 조회
+        String query = String.format("SELECT * FROM jdbc_students WHERE id = '%s'", id);
+        log.debug("Select query: {}", query);
 
+        try(Connection conn = DbUtils.getConnection();
+            Statement statement = conn.createStatement();
+            ResultSet resultSet = statement.executeQuery(query)) {
+            if(resultSet.next()) {
+                Student student = new Student(resultSet.getString("id"),
+                        resultSet.getString("name"),
+                        Student.GENDER.valueOf(resultSet.getString("gender")),
+                        resultSet.getInt("age"),
+                        resultSet.getTimestamp("created_at").toLocalDateTime()
+                );
+                return Optional.of(student);
+            }
+        }catch(SQLException e) {
+            throw new RuntimeException(e);
+        }
         return Optional.empty();
     }
 
     @Override
     public int update(Student student){
         //todo#3 student 수정, name <- 수정합니다.
+        String query = String.format("UPDATE jdbc_students SET name='%s', gender='%s', age='%d' WHERE id = '%s'",
+                student.getName(),
+                student.getGender(),
+                student.getAge(),
+                student.getId()
+        );
+        log.debug("Update query: {}", query);
 
-        return 0;
+        try(Connection conn = DbUtils.getConnection();
+            Statement statement = conn.createStatement();) {
+            int result = statement.executeUpdate(query);
+            log.debug("Update result: {}", result);
+            return result;
+        }catch(SQLException e){
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public int deleteById(String id){
        //todo#4 student 삭제
+        String query = String.format("DELETE FROM jdbc_students WHERE id = '%s'", id);
+        log.debug("Delete query: {}", query);
 
-        return 0;
+        try(Connection conn = DbUtils.getConnection();
+        Statement statement = conn.createStatement();) {
+            int result = statement.executeUpdate(query);
+            log.debug("Delete result: {}", result);
+            return result;
+        }catch(SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
